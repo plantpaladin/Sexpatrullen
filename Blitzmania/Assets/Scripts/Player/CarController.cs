f73774685a40829c5db5446e21bbc9b5c27782ab
K3ikoku <wall.gustaf@gmail.com>
2016-02-09 15:53:04 +0100

    Added scripts for player controller and car controller

---
+++ Blitzmania/Assets/Scripts/Player/CarController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] private float m_currentSpeed = 0f;
+    [SerializeField] private float m_tourque = 50f;
+    [SerializeField] private float m_maxSpeed = 200f;
+    [SerializeField] private float m_brakeSpeed = 0f;
+    [SerializeField] private float m_maxSteer = 25f;
+    private Rigidbody m_rigidbody;
+
+
+    void Awake()
+    {
+        m_rigidbody =  this.GetComponent<Rigidbody>();
+    }
+	// Use this for initialization
+	void Start ()
+    {
+        m_rigidbody.centerOfMass = new Vector3(0f, -0.5f, 0.3f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void Move(float steering, float accel, float brake)
+    {
+        m_currentSpeed = (m_currentSpeed + 1) * 0.5f;
+        m_currentSpeed = m_currentSpeed * m_tourque * Time.deltaTime * 100f;
+        // Keep writing code here
+
+    }
+}
---
+++ Blitzmania/Assets/Scripts/Player/PlayerUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class PlayerUI : MonoBehaviour
+{
+    [RequireComponent(typeof(CarController))]
+    public class CarUserControl : MonoBehaviour
+    {
+
+        [SerializeField] private string m_steeringInput = "WASD Steering";
+        [SerializeField] private string m_accelInput = "WASD Acceleration";
+        [SerializeField] private string m_brakeInput = "WASD Brake";
+
+        private CarController m_Car; // the car controller we want to use
+
+
+        private void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+        }
+
+
+        private void FixedUpdate()
+        {
+            // pass the input to the car!
+            float h = CrossPlatformInputManager.GetAxis(m_steeringInput);
+            float a = CrossPlatformInputManager.GetAxis(m_accelInput);
+            float b = CrossPlatformInputManager.GetAxis(m_brakeInput);
+            m_Car.Move(h, a, b);
+
+        }
+    }
+}
