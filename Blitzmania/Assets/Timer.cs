8fb31fa7e3c886096df75fca6290a1ef4d768a05
plantpaladin <plantpaladin@gmail.com>
2016-02-10 19:24:46 +0100

    Attempt to add more functionality
    I try to add so the script shall write out the entier inormation at once
    but something goes wrong, gonna check it tomorrow

--- Blitzmania/Assets/Timer.cs
+++ Blitzmania/Assets/Timer.cs
@@ -4,7 +4,9 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour {
     private Text m_diplayTime;
     private float m_timer;
-
+    private int m_numbPlayers;
+    private int m_playerWithCrown;//gives the number of the player
+    private System.Collections.Generic.List<float> m_playerScoreList;
 	// Use this for initialization
 	void Start () {
         m_diplayTime = GetComponent<Text>();
@@ -14,17 +16,42 @@ public class Timer : MonoBehaviour {
         }
         else
         {
-            m_timer = 300;//just a default value
+            m_timer = 300;//or is set to a default value
         }
-	}
+        
+        if (PlayerPrefs.HasKey("numberOfPlayers"))//same as previous
+        {
+            m_numbPlayers = PlayerPrefs.GetInt("numberOfPlayers");//gets the number of players
+        }
+        else
+        {
+            m_numbPlayers = 3;//or is set to a default value
+        }
+        for (int i = 0; i < m_numbPlayers; i++)//runs the script once for each player
+        {
+            m_playerScoreList.Add(0.0f);//all of them start at zero score
+        }//the players score is placed at index playerNumber-1 (because there is no player 0)
+        m_playerWithCrown = 1;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        
-        m_timer -= Time.deltaTime;//removes the time since last update
-        if (m_timer > 0)
+        m_timer -= Time.deltaTime; ;//removes the time since last update
+        for (int i = 0; i < m_playerScoreList.Count; i++)
         {
-            m_diplayTime.text = "Time Left = " + m_timer.ToString();
+            if (i == (m_playerWithCrown - 1))//decrease by one as there is still no player 0
+            {
+                m_playerScoreList[i] += Time.deltaTime; ;
+            }
+        }
+        if (m_timer > 0)//has to make check after or the last score wont be added
+        {
+            m_diplayTime.text = "Time Left = " + m_timer.ToString() + "\n";//the \n means it's a new line
+            for (int i = 0; i < m_playerScoreList.Count; i++)
+            {
+                m_diplayTime.text += "Player " + (i + 1) + " score equals "+ m_playerScoreList[i]  + "\n";
+            }
         }
         else
         {
